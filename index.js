d5a7587c5357d42fb608eee93e7d4230d11d1c4c
stvik <stvik2@gmail.com>
2019-12-10 11:31:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
-
+	return fetch('https://anapioficeandfire.com/api/books')
+		.then(resp => resp.json())
+		.then(json => renderBooks(json))
+		// .then(json => fifthBook(json))
 }
 
 function renderBooks(json) {
@@ -9,7 +12,16 @@ function renderBooks(json) {
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
+  	console.log(json)
+ 	let fifthBook = json[5].name
+ 	console.log(fifthBook)
+ 	let total = 0
+ 	json.forEach(book => total = total + book.numberOfPages)
+ 	console.log(total)
+
 }
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
